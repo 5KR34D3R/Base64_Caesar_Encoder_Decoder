63915fbb31d1a540236e47c194e5d27f120f2392
5KR34D3R <g.trebo@gmx.at>
2021-03-07 20:17:39 +0100

    V0.2

--- src/Encoder.java
+++ src/Encoder.java
@@ -1,15 +1,40 @@
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 public class Encoder {
+	private static char [] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	
 	public static String encodeString(String encodedString) {
-		try {
-			//return new String(Base64.getDecoder().decode((encodedString.substring(0,encodedString.length()-1))));
+		try {			
 			return Base64.getEncoder().encodeToString(encodedString.getBytes());
 		}catch (Exception e) {
 			System.err.println(e);
 			return null;
 		}		 
+	}
+	public static List<String> ceasarEncoder(String decodedString) {	
+		decodedString = decodedString.toLowerCase();
+		List<String> results = new ArrayList<String>();		
+		String currentResult = "";		
+		
+		for(int offset = 1; offset <= alphabet.length; offset ++) {		
+			for(int e = 0; e < decodedString.length(); e++) {				
+				for(int current = 0; current < alphabet.length; current++) {							
+					if(Character.valueOf(decodedString.charAt(e)).equals(alphabet[current])) {						
+						if((current+offset) >= 26) {							
+							currentResult += String.valueOf(alphabet[(current+offset-26)]);							
+						}else {							
+							currentResult += String.valueOf(alphabet[(current+offset)]);														
+						}	
+						break;
+					}
+				}				
+			}
+			results.add(currentResult);
+			currentResult = "";
+		}		
+		return results;
 	}
 
 }
